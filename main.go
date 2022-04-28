b9119c7450cce2e72839f43cc618769f5198c1b4
NSP22SCM14G <nguramkonda@hawk.iit.edu>
2022-04-28 10:28:39 -0500

    Update main.go

--- main.go
+++ main.go
@@ -183,7 +183,6 @@ func main() {
 	// To find your Postgres container IP, use the command with your network name listed in the docker compose file as follows: 
 	// docker network inspect cbi_backend
 	//db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=172.19.0.2 sslmode=disable port = 5433"
-	
 	//Option 4
 	//Database application running on Google Cloud Platform. 
 	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/chicago-business-intelligence:us-central1:mypostgres sslmode=disable port = 5432"
